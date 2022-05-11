61343da7a431daedec0c9592db1c8a28f65dfc9b
Yogesh <borole116@gmail.com>
2022-05-11 09:09:43 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Calculator';
+  title = 'Calculator-App';
+
+  public num1!: number;
+  public num2!: number;
+  public result!: number;
+
+  add(){
+    this.result= this.num1 + this.num2;
+  }
+  sub(){
+    this.result= this.num1-this.num2;
+  }
+  mul(){
+    this.result= this.num1*this.num2;
+  }
+  div(){
+    this.result= this.num1/this.num2;
+  }
 }
+
